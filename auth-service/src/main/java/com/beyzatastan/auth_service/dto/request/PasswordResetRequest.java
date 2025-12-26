75ccc6a04ce495857a95982d8dcd151c5af1ed30
Beyza Taştan <bbeyzatastan@gmail.com>
2025-12-26 23:50:39 +0300

    auth-service-dtos-added-bt

---
+++ auth-service/src/main/java/com/beyzatastan/auth_service/dto/request/PasswordResetRequest.java
@@ -0,0 +1,27 @@
+package com.beyzatastan.auth_service.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasswordResetRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Reset token is required")
+    private String resetToken;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    private String newPassword;
+}
