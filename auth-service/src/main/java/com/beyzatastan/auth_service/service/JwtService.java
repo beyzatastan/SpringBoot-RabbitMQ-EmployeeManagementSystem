22c18220d947404ac972cca020a93861e922069a
Beyza Taştan <bbeyzatastan@gmail.com>
2026-01-02 23:27:51 +0300

    auth-service-jwt-config-done

--- auth-service/src/main/java/com/beyzatastan/auth_service/service/JwtService.java
+++ auth-service/src/main/java/com/beyzatastan/auth_service/service/JwtService.java
@@ -5,4 +5,6 @@ import com.beyzatastan.auth_service.entity.User;
 public interface JwtService {
     String generateAccessToken(User user);
     boolean isTokenValid(String token);
+    String extractUsername(String token);
+    String generateRefreshToken(User user);
 }
--- auth-service/src/main/java/com/beyzatastan/auth_service/service/impl/JwtServiceImpl.java
+++ auth-service/src/main/java/com/beyzatastan/auth_service/service/impl/JwtServiceImpl.java
@@ -2,10 +2,19 @@ package com.beyzatastan.auth_service.service.impl;
 
 import com.beyzatastan.auth_service.entity.User;
 import com.beyzatastan.auth_service.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -20,11 +29,54 @@ public class JwtServiceImpl implements JwtService {
 
     @Override
     public String generateAccessToken(User user) {
-        return "";
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", user.getId());
+        claims.put("role", user.getRole());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    @Override
+    public String generateRefreshToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 604800000L))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //tokendan kullanıcı adını al
+    @Override
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
     }
 
     @Override
     public boolean isTokenValid(String token) {
-        return false;
+        try {
+            return !extractAllClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
